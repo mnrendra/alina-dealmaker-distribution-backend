9385c15257cdcd0bcbae60e94e8edaa664bd4868
Muhammad Rendra <rendra@garuda.io>
2020-12-16 21:15:36 +0700

    add hallo.js

---
+++ hallo.js
@@ -0,0 +1,32 @@
+const express = require('express')
+// const bodyParser = require('body-parser')
+// const helmet = require('helmet')
+// const compression = require('compression')
+// const cors = require('cors')
+
+// const initSocketIO = require('./sockets')
+// const { invalidJsonHandler } = require('./errors')
+// const routes = require('./routes')
+
+const app = express()
+// const { server, io } = initSocketIO(app)
+
+// app.use(cors())
+// app.use(helmet())
+// app.use(compression())
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: true }))
+// app.use(express.json())
+// app.use(invalidJsonHandler)
+// app.use(routes(io))
+
+// module.exports = server
+app.get('/', (req, res) => {
+  res.status(200).send({
+    hallo: 'bro!'
+  })
+})
+
+app.listen(8080, () => {
+  console.log('listen on port 8080')
+})
